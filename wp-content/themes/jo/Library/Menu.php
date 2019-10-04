5df2b150518d0273068677928d113e28271495c0
iordanis georgiadis <iordanhsdec92@gmail.com>
2019-10-04 16:34:36 +0300

    Improve custom menu

--- wp-content/themes/jo/Library/Menu.php
+++ wp-content/themes/jo/Library/Menu.php
@@ -28,14 +28,17 @@ class CustomMenu extends \Walker_Nav_Menu {
   function start_el(&$output, $item, $depth=0, $args=array(), $id = 0) {
   	static $itemCounter = 0;
     $itemClasses = '';
-
+    //echo '<pre>';print_r($item);echo '</pre>';
     if($itemCounter < $this->maxMenuItems){
 
      foreach ($item->classes as $key => $value) {
      	$itemClasses .= $value.' ';
      }
-  	 $output .= '<ul>';	
-  	 $output .= "<li class='".$itemClasses."'>".$item->post_title."</li>";
+	 
+	 $title = $this->getInfoFromItem($item);
+	 if($title){
+	  $output .= "<li class='".$itemClasses."'>".$title."</li>";	
+	 }
     }
 
   	if($item->menu_item_parent == 0){
@@ -44,7 +47,17 @@ class CustomMenu extends \Walker_Nav_Menu {
   }
 
   function end_el(&$output, $item, $depth=0, $args=array(), $id = 0) {
-  	$output .= '</ul>';
+
   }
   
+  function getInfoFromItem($item){
+
+  	if($item->post_title !== ''){
+  		return $item->post_title;
+  	}elseif($item->title !== ''){
+  		return $item->title;
+  	}else{
+  		return false;
+  	}
+  }
 }
